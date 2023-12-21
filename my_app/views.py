ce091ad7c36ce48ab3b05b9eb8c91410d3c0c6db
parin541 <116267874+parin541@users.noreply.github.com>
2023-12-21 12:09:48 -0600

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, Everyone from django app!')
+    return HttpResponse('Hello, Everyone from django aplication !')
 
