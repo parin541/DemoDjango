f6ccc05bb273f32b54a61e1158165ae125359951
parin541 <116267874+parin541@users.noreply.github.com>
2024-02-02 15:06:13 -0600

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, Everyone from django application Update')
+    return HttpResponse('Hello, Everyone from django application Update from Webyops')
 
